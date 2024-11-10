3d5e9c976653c13f54f5b433b39b169eaddd3556
BryanttV <bryanttandrei2015@gmail.com>
2024-11-10 17:57:57 -0500

    test(16i): add tests for removeFromCart function

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,4 +1,9 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import {
+  addToCart,
+  cart,
+  loadFromStorage,
+  removeFromCart,
+} from "../../data/cart.js";
 
 describe("test suite: addToCart", () => {
   beforeEach(() => {
@@ -40,6 +45,55 @@ describe("test suite: addToCart", () => {
 
     loadFromStorage();
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6", 1);
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "cart",
+      JSON.stringify([
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ])
+    );
+    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart[0].quantity).toEqual(1);
+  });
+});
+
+describe("test suite: RemoveFromCart", () => {
+  beforeEach(() => {
+    spyOn(localStorage, "setItem");
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ]);
+    });
+  });
+
+  it("removes a productId that is in the cart", () => {
+    loadFromStorage();
+
+    removeFromCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+
+    expect(cart.length).toEqual(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "cart",
+      JSON.stringify([])
+    );
+  });
+
+  it("removes a productId that is not in the cart", () => {
+    loadFromStorage();
+
+    removeFromCart("15b6fc6f-327a-4ec4-896f-486349e85a3d");
+
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(localStorage.setItem).toHaveBeenCalledWith(
