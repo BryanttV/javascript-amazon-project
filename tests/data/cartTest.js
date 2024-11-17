06e11ffec72cfcf5418bb40b36588955291025d4
BryanttV <bryanttandrei2015@gmail.com>
2024-11-16 19:27:05 -0500

    test(16l): add test when product does not exist in the cart

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -147,4 +147,16 @@ describe("test suite: updateDeliveryOption", () => {
       ])
     );
   });
+
+  it("does nothing if the product is not in the cart", () => {
+    loadFromStorage();
+
+    updateDeliveryOption("15b6fc6f-327a-4ec4-896f-486349e85a3d", "2");
+
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual("1");
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+  });
 });
